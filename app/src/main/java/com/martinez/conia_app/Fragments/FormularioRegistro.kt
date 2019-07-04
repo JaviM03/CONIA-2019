152a84e0f6f1d9aa793242aa6dbf3becd0257d7e
joisgori <00034715@uca.edu.sv>
2019-07-04 00:32:10 -0600

    Agrego la lógica para el formulario

--- app/src/main/java/com/martinez/conia_app/Fragments/FormularioRegistro.kt
+++ app/src/main/java/com/martinez/conia_app/Fragments/FormularioRegistro.kt
@@ -1,6 +1,7 @@
 package com.martinez.conia_app.Fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,18 +10,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.martinez.conia_app.Entidad.User
 import com.martinez.conia_app.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_formulario_registro.*
 
 
 class FormularioRegistro : Fragment() {
-
     private lateinit var auth : FirebaseAuth
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_formulario_registro, container, false)
     }
@@ -31,17 +33,15 @@ class FormularioRegistro : Fragment() {
         btn_create.setOnClickListener {
             if (!ctr_email.text.toString().isEmpty() && !ctr_pass.text.toString().isEmpty()){
                 auth.createUserWithEmailAndPassword(ctr_email.text.toString(), ctr_pass.text.toString())
-                    .addOnCompleteListener {
-                        //Acá la lógica para verificar que los datos coincidan con el usuario que cree en la base de firebase
-                        if(!it.isSuccessful) return@addOnCompleteListener
-                        //Sí es el correcto, pasa a la siguiente instancia, sino
-                        view.findNavController().navigate(R.id.action_formularioRegistro_to_menu_home)
-                        //Log.d("El usuario ingresado es correcto", it.result.toString())
-                        (this.activity as AppCompatActivity).bottom_nav_view.visibility = View.VISIBLE
-                    }.addOnFailureListener {
-                        //Le mando un toast con la validación pura de firebase para indicar qué es lo incorrecto
-                        Toast.makeText(view!!.context, "Error al crear usuario", Toast.LENGTH_LONG).show()
-                    }
+                        .addOnCompleteListener {
+                            //Acá la lógica para verificar que los datos coincidan con el usuario que cree en la base de firebase
+                            if(!it.isSuccessful) return@addOnCompleteListener
+                            registerOnFirebaseDatabase(view, ctr_email.text.toString(),ctr_name.text.toString(), ctr_user.text.toString())
+
+                        }.addOnFailureListener {
+                            //Le mando un toast con la validación pura de firebase para indicar qué es lo incorrecto
+                            Toast.makeText(view!!.context, "Error al crear usuario", Toast.LENGTH_LONG).show()
+                        }
             }else{
                 Toast.makeText(view!!.context, "Por favor rellenar TODOS los campos", Toast.LENGTH_LONG).show()
             }
@@ -53,5 +53,24 @@ class FormularioRegistro : Fragment() {
         }
     }
 
+    private fun registerOnFirebaseDatabase(view:View, name:String, username:String, email:String){
+
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val reference = FirebaseDatabase.getInstance().getReference("/users/$uid")
+
+        val user = User(name, username, email)
+
+        reference.setValue(user)
+                .addOnSuccessListener {
+                    Log.d("Register", "Guardando datos en la base de datos")
+
+                    view.findNavController().navigate(R.id.action_formularioRegistro_to_menu_home)
+
+                    (this.activity as AppCompatActivity).bottom_nav_view.visibility = View.VISIBLE
+                }.addOnFailureListener{
+                    Log.d("Register", "Fallo al guardar (setear) valores en la base de datos: ${it.message}")
+                }
+
+    }
 
 }
